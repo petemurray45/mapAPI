367afed324bc344634265fbe983aac7870c1e977
petemurray45 <pmurray697@gmail.com>
2025-05-28 15:26:29 +0100

    finsihed route integration, added transport selection dropdown

--- frontend/src/components/MapBox.jsx
+++ frontend/src/components/MapBox.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Map, MapStyle, config } from "@maptiler/sdk";
+import { Map, MapStyle, config, coordinates } from "@maptiler/sdk";
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 import axios from "axios";
 
@@ -18,6 +18,15 @@ function MapBox() {
   const zoom = 13;
 
   useEffect(() => {
+    const testGeocode = async () => {
+      const url = `https://api.openrouteservice.org/geocode/search?api_key=${orsKey}&text=Belfast City Hall&size=1`;
+      const res = await axios.get(url);
+      console.log("Testing geocode response", res.data);
+    };
+    testGeocode();
+  }, []);
+
+  useEffect(() => {
     if (!map.current) {
       console.log("creating map....");
       map.current = new Map({
@@ -28,6 +37,75 @@ function MapBox() {
       });
     }
   }, [belfast.lng, belfast.lat, zoom]);
+
+  const handleRoute = async () => {
+    try {
+      const originCoords = await geoCode(origin);
+      const destinationCoords = await geoCode(destination);
+
+      console.log("Origin input:", origin);
+      console.log("Origin coordinates:", originCoords);
+      console.log("Destination input:", destination);
+      console.log("Destination coordinates:", destinationCoords);
+
+      if (!originCoords || !destinationCoords) {
+        alert("Could not find one or both locations");
+        return;
+      }
+
+      const res = await axios.post(
+        "https://api.openrouteservice.org/v2/directions/driving-car/geojson",
+        {
+          coordinates: [originCoords, destinationCoords],
+        },
+        {
+          headers: {
+            Authorization: orsKey,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const geojson = res.data;
+
+      if (map.current.getSource("route")) {
+        map.current.getSource("route").setData(geojson);
+      } else {
+        map.current.addSource("route", {
+          type: "geojson",
+          data: geojson,
+        });
+        map.current.addLayer({
+          id: "route",
+          type: "line",
+          source: "route",
+          paint: {
+            "line-color": "#3b82f6",
+            "line-width": 4,
+          },
+        });
+      }
+    } catch (err) {
+      console.log("Something went wrong in handle route function", err);
+    }
+  };
+
+  const geoCode = async (text) => {
+    const url = `https://api.openrouteservice.org/geocode/search?api_key=${orsKey}&text=${encodeURIComponent(
+      text
+    )}&size=1`;
+
+    try {
+      const res = await axios.get(url);
+      const coords = res.data.features?.[0]?.geometry?.coordinates;
+
+      if (!coords) throw new Error("No results found for: ", text);
+      return coords; // [long, lat]
+    } catch (err) {
+      console.log("Something went wrong in geoCode function ", err);
+      return null;
+    }
+  };
 
   return (
     <div className="flex items-center justify-center px-20 min-h-screen">
@@ -42,11 +120,22 @@ function MapBox() {
           {/*Inputs and labels*/}
           <div className="flex flex-col justify-center gap-4">
             <div>
+              <select className="select select-bordered w-full max-w">
+                <option disabled selected>
+                  Transport Method
+                </option>
+                <option>Walking</option>
+                <option>Driving</option>
+                <option>Cycling</option>
+              </select>
+            </div>
+            <div>
               <label className="label font-semibold">Origin Location</label>
               <input
                 type="text"
                 placeholder="Enter origin"
                 className="input input-bordered w-full mt-1"
+                onChange={(e) => setOrigin(e.target.value)}
               />
             </div>
 
@@ -56,10 +145,14 @@ function MapBox() {
                 type="text"
                 placeholder="Enter destination"
                 className="input input-bordered w-full mt-1"
+                onChange={(e) => setDestination(e.target.value)}
               />
             </div>
 
-            <button className="btn btn-is-primary shadow-xl mt-6">
+            <button
+              className="btn btn-is-primary shadow-xl mt-6"
+              onClick={handleRoute}
+            >
               Show Route
             </button>
           </div>
